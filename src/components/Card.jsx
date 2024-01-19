45a3f8e38d181194bc490e901f5e1728bbbc7647
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2024-01-19 23:52:54 +0900

    [add] card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Card({result}) {
+  return (
+    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg transition-shadow duration-200'>
+        <Link href={`/album/${result.id}`}>
+            <Image 
+                src={result.images.at(0).url} 
+                width={500} 
+                height={300}
+                className='group-hover:opacity-75 transition-opacity duration-300'></Image>
+        </Link>
+    </div>
+  )
+}
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable react/jsx-key */
+import Card from "./card";
+import Image from "next/image";
+
 export default function Results({results}) {
   return (
-    <div>
-        {
-            results.map((result) => (
-                <div key={result.id}>
-                    <h2>{result.name}</h2>
-                </div>
-            ))
-        }
+    <div className="sm:grid sm:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl
+    mx-auto py-4">
+        {results.map((result) => (
+            <Card key={result.id} result={result} />
+        ))}
     </div>
   )
 }
